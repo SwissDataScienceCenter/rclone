f8da4083352cd569198b1d5b733923a3503bf75d
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-03-31 10:21:35 +0200

    lint

--- backend/doi/api/inveniotypes.go
+++ backend/doi/api/inveniotypes.go
@@ -2,22 +2,22 @@
 
 package api
 
-// Representation of a record stored in InvenioDRM
+// InvenioRecordResponse si the representation of a record stored in InvenioDRM
 type InvenioRecordResponse struct {
 	Links InvenioRecordResponseLinks `json:"links"`
 }
 
-// Representation of a record's links
+// InvenioRecordResponseLinks represents of a record's links
 type InvenioRecordResponseLinks struct {
 	Self string `json:"self"`
 }
 
-// Representation of a record's files
+// InvenioFilesResponse is the representation of a record's files
 type InvenioFilesResponse struct {
 	Entries []InvenioFilesResponseEntry `json:"entries"`
 }
 
-// Representation of a file entry
+// InvenioFilesResponseEntry is the representation of a file entry
 type InvenioFilesResponseEntry struct {
 	Key      string                         `json:"key"`
 	Checksum string                         `json:"checksum"`
@@ -27,7 +27,7 @@ type InvenioFilesResponseEntry struct {
 	Links    InvenioFilesResponseEntryLinks `json:"links"`
 }
 
-// File links details
+// InvenioFilesResponseEntryLinks represents file links details
 type InvenioFilesResponseEntryLinks struct {
 	Content string `json:"content"`
 }
